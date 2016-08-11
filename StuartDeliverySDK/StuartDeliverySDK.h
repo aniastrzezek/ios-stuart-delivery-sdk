432ce1c8bd85eee8287b4fea664e3b5cf4d1a517
Anna Strzeżek <astrzezek@gmail.com>
2016-08-11 21:23:06 +0200

    Initial Commit

---
+++ StuartDeliverySDK/StuartDeliverySDK.h
@@ -0,0 +1,19 @@
+//
+//  StuartDeliverySDK.h
+//  StuartDeliverySDK
+//
+//  Created by Anna Strzezek on 11/08/2016.
+//  Copyright © 2016 Anna Strzeżek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StuartDeliverySDK.
+FOUNDATION_EXPORT double StuartDeliverySDKVersionNumber;
+
+//! Project version string for StuartDeliverySDK.
+FOUNDATION_EXPORT const unsigned char StuartDeliverySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StuartDeliverySDK/PublicHeader.h>
+
+
